e910dafbad17e22167d08fd5af88517f54597420
Jianwei Han <hanjianwei@gmail.com>
2024-07-19 08:46:19 +0800

    Modify install path

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ ! -d $HOME/.cargo ]]; then
+export RUSTUP_HOME=$HOME/Applications/rust/rustup
+export CARGO_HOME=$HOME/Applications/rust/cargo
+
+if [[ ! -d $CARGO_HOME ]]; then
   export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
   export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
