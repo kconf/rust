2771c7f7ad18862173075494ccc3e65b51413392
Jianwei Han <hanjianwei@gmail.com>
2024-07-19 08:49:02 +0800

    Modify install path

--- .zsh.d/rust.sh
+++ .zsh.d/rust.sh
@@ -1,3 +1,6 @@
+export RUSTUP_HOME=$HOME/Applications/rust/rustup
+export CARGO_HOME=$HOME/Applications/rust/cargo
 export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
-source $HOME/.cargo/env
+
+source $CARGO_HOME/env
