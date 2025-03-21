3096c850414baad5db1d8a36f9e96e83dc734d68
Jianwei Han <hanjianwei@gmail.com>
2025-03-21 10:37:14 +0800

    feat(app): Use default install for rust
    Changelog: feature

--- .zsh.d/rust.sh
+++ .zsh.d/rust.sh
@@ -1,6 +1,3 @@
-export RUSTUP_HOME=$HOME/Applications/rust/rustup
-export CARGO_HOME=$HOME/Applications/rust/cargo
-export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
-export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
-
-source $CARGO_HOME/env
+if [ -d $HOME/.cargo ]; then
+  source $HOME/.cargo/env
+fi
