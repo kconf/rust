1139ca983b721652bd35f8c47d3638e162a8e250
Jianwei Han <hanjianwei@gmail.com>
2025-03-21 10:37:52 +0800

    feat(app): Use default installation
    Changelog: feature

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-export RUSTUP_HOME=$HOME/Applications/rust/rustup
-export CARGO_HOME=$HOME/Applications/rust/cargo
-
-if [[ ! -d $CARGO_HOME ]]; then
-  export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
-  export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
